dec394c6b1727ecdee723f042a5dfb1a010b080b
encarro12 <ecarrogarrido12@gmail.com>
2023-02-06 20:39:12 +0100

    Function equanimity implemented, no errors detected

---
+++ equanimity.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float equanimity(string f) {
+    int I = 0;
+    for(int i = 1; i <= 5; i++)
+        for (int j = 0; j < pow(2, 5); j += pow(2, i))
+            for (int k = 0; k < pow(2, i - 1); k++)
+                if(f[k + j] != f[(k + j) + pow(2, i - 1)])
+                    I++;
+    return (I + 0.0)/(5*16);
+}
+
+int main() {
+    cout << equanimity("01101001100101101001011001101001") << endl;
+    system("pause");
+    return 0;
+}
